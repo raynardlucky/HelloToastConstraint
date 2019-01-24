52cef9bee70f9e14ad867170b84eaaea4df592a4
raynardlucky <rayluck25@gmail.com>
2019-01-24 18:00:51 +0700

    Initial commit

--- app/src/main/java/raynard/exercise/com/hellotoast/MainActivity.java
+++ app/src/main/java/raynard/exercise/com/hellotoast/MainActivity.java
@@ -14,17 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mShowCount  =  (TextView) findViewById(R.id.show_count);
+        mShowCount = (TextView) findViewById(R.id.show_count);
     }
 
     public void showToast(View view) {
-        Toast toast = Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
         toast.show();
     }
 
     public void countUp(View view) {
         mCount++;
-        if (mShowCount != null){
+        if (mShowCount != null) {
             mShowCount.setText(Integer.toString(mCount));
         }
     }
